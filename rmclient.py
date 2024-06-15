6eb10d464e2969b2c13a4899b9385a9c6d911c9b
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-06-15 13:38:08 +0200

    Added bindings and modes.

--- rmclient.py
+++ rmclient.py
@@ -1,7 +1,17 @@
+from screens.issues import IssuesScreen
 from textual.app import App
 
+
 class RMClient(App):
-    pass
+    BINDINGS = [
+        ("i", "switch_mode('issues')", "Issues")
+    ]
+    MODES = {
+        "issues": IssuesScreen
+    }
+
+    def on_mount(self) -> None:
+        self.switch_mode("issues")
 
 
 if __name__ == "__main__":
