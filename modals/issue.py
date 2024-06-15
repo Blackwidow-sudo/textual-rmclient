8746a98860b7a410e24f6db14e99665335274d91
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-06-15 14:36:48 +0200

    Added fields to issue modal.

--- modals/issue.py
+++ modals/issue.py
@@ -1,13 +1,21 @@
 from textual.app import ComposeResult
 from textual.containers import Grid
 from textual.screen import ModalScreen
-from textual.widgets import Button, Label
+from textual.widgets import Button, Label, Input, Select
 
 
 class IssueModal(ModalScreen):
+    ACTIVITIES = (
+        ("Development", 0),
+    )
+
     def compose(self) -> ComposeResult:
         yield Grid(
             Label("Issue", id="issue"),
+            Input("Date", id="date"),
+            Input("Hours", id="hours"),
+            Select(self.ACTIVITIES, id="activity"),
+            Input("Comment", id="comment"),
             Button("Close", id="close", variant="primary"),
             Button("Start", id="start", variant="error"),
             id="dialog",
