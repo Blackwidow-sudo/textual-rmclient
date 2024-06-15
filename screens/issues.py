c0c5061f11f38103bf1b3d41ff8f87c2d6f989cb
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-06-15 13:53:08 +0200

    Added pretty print of redmine issues.

--- screens/issues.py
+++ screens/issues.py
@@ -1,9 +1,17 @@
+import requests
 from textual.app import ComposeResult
 from textual.screen import Screen
-from textual.widgets import Footer, Placeholder
+from textual.widgets import Footer, Pretty
 
 
 class IssuesScreen(Screen):
     def compose(self) -> ComposeResult:
-        yield Placeholder("Issues Screen")
+        issues = self.get_issues()
+
+        yield Pretty(issues)
         yield Footer()
+
+    def get_issues(self):
+        response = requests.get("https://redmine.org/issues.json")
+
+        return response.json()
