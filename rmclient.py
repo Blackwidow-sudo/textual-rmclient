0cc9e076bf38a8447c502d26d988d76a45b649d8
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-06-15 14:11:57 +0200

    Added quit mode.

--- rmclient.py
+++ rmclient.py
@@ -1,14 +1,20 @@
+from modals.quit import QuitModal
 from screens.issues import IssuesScreen
 from textual.app import App
 
 
 class RMClient(App):
     BINDINGS = [
-        ("i", "switch_mode('issues')", "Issues")
+        ("i", "switch_mode('issues')", "Issues"),
+        ("q", "request_quit", "Quit"),
     ]
     MODES = {
-        "issues": IssuesScreen
+        "issues": IssuesScreen,
+        "quit": QuitModal,
     }
+
+    def action_request_quit(self) -> None:
+        self.push_screen(QuitModal())
 
     def on_mount(self) -> None:
         self.switch_mode("issues")
