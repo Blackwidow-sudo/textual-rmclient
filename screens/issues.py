0c4a412c3be90b60215083ac8b9440cacaf973b0
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2024-06-15 14:19:50 +0200

    Added data table to issues screen.

--- screens/issues.py
+++ screens/issues.py
@@ -1,17 +1,42 @@
 import requests
 from textual.app import ComposeResult
 from textual.screen import Screen
-from textual.widgets import Footer, Pretty
+from textual.widgets import DataTable, Footer
 
 
 class IssuesScreen(Screen):
-    def compose(self) -> ComposeResult:
-        issues = self.get_issues()
+    COLUMNS = (
+        "ID",
+        "Project",
+        "Tracker",
+        "Status",
+        "Priority",
+        "Subject",
+    )
 
-        yield Pretty(issues)
+    def compose(self) -> ComposeResult:
+        yield DataTable()
         yield Footer()
 
     def get_issues(self):
         response = requests.get("https://redmine.org/issues.json")
 
-        return response.json()
+        if response.status_code == 200:
+            return response.json()["issues"]
+
+        return []
+
+    def on_mount(self) -> None:
+        issues = self.get_issues()
+        table = self.query_one(DataTable)
+        table.add_columns(*self.COLUMNS)
+
+        for issue in issues:
+            table.add_row(
+                issue["id"],
+                issue["project"]["name"],
+                issue["tracker"]["name"],
+                issue["status"]["name"],
+                issue["priority"]["name"],
+                issue["subject"],
+            )
